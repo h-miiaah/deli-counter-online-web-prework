1fc4b2e519603ec3a240fc1e28f041ae24f06bde
Haim D <haim11293@aol.com>
2019-06-28 00:25:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,2 +1,14 @@
 # Write your code here.
 katz_deli = []
+
+def line(array_of_customers)
+  if array_of_customers.length == 0
+    puts "The line is currently empty."
+  else
+    message = "The line is currently:"
+    array_of_customers.each_with_index do |customer, index|
+      message += " #{index.to_i + 1}. #{customer}"
+  end
+  puts "#{message}"
+end
+
